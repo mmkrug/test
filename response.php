95ef3124a18e3588a2d51a3b479cd2932c7c1acf
mmkrug <44449460+mmkrug@users.noreply.github.com>
2020-12-14 14:27:24 +0100

    Update response.php

--- response.php
+++ response.php
@@ -1,15 +1,7 @@
 <?php
+    header("content-type: text/xml");
+?>
 
-//namespace App\Controller;
-
-use Twilio\TwiML\MessagingResponse;
-
-// Set the content-type to XML to send back TwiML from the PHP Helper Library
-header("content-type: text/xml");
-
-$response = new MessagingResponse();
-$response->message(
-    "I'm using the Twilio PHP library to respond to this SMS!"
-);
-
-echo $response;
+<Response>  
+    <Message>Thx for Message</Message>
+</Response>
